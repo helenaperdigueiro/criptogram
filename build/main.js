44e8f1de80aefde405f818064569f7a458865df5
Helena Perdigueiro <helena.perdigueiro@gmail.com>
2023-01-17 19:37:50 -0300

    versao 1.0.0

--- build/main.js
+++ build/main.js
@@ -1,27 +1,71 @@
 "use strict";
-var container = window.document.getElementById("container");
-var charCodes = Array.from(Array(26)).map(function (_element, index) { return index + 65; });
-var alphabet = charCodes.map(function (charCode) { return String.fromCharCode(charCode); });
+var _a;
+const container = document.getElementById("container");
+const charCodes = Array.from(Array(26)).map((_element, index) => index + 65);
+const alphabet = charCodes.map(charCode => String.fromCharCode(charCode));
 console.log(alphabet);
-var icons = ["avocado.png", "baguette.png", "banana.png", "barley.png", "biscuit.png", "burger.png", "capsicum.png", "carrot.png", "cauliflower.png",
+const icons = ["avocado.png", "baguette.png", "banana.png", "barley.png", "biscuit.png", "burger.png", "capsicum.png", "carrot.png", "cauliflower.png",
     "cheese-slice.png", "chilli.png", "chocolate.png", "cutlery.png", "disposable.png", "donut.png", "eggplant.png", "fried-fish.png", "honey-dipper.png", "ice-cream.png",
     "lobster.png", "peas.png", "pizza-slice.png", "toaster.png", "turkey.png", "turnip.png", "waffle.png"];
 console.log(icons);
-var charsIcons = new Map();
-alphabet.forEach(function (char, index) { return charsIcons.set(char, icons[index]); });
+const charsIcons = new Map();
+alphabet.forEach((char, index) => charsIcons.set(char, icons[index]));
 console.log(charsIcons);
-var printKey = function () {
-    var key = document.getElementById("key");
-    charsIcons.forEach(function (icon, char) {
-        var div = document.createElement("div");
-        var letter = document.createElement("h6");
+(_a = document.getElementById("form")) === null || _a === void 0 ? void 0 : _a.addEventListener("submit", (event) => {
+    event.preventDefault();
+    generateCriptogram();
+});
+const generateCriptogram = () => {
+    var _a;
+    const phrase = (_a = document.getElementById("input-phrase")) === null || _a === void 0 ? void 0 : _a.value.toUpperCase();
+    const phraseChars = [...phrase];
+    const criptogramContainer = document.getElementById("criptogram-container");
+    const criptogram = document.createElement("div");
+    criptogram.setAttribute("class", "criptogram");
+    let word = document.createElement("div");
+    word.setAttribute("class", "word");
+    phraseChars.forEach(char => {
+        if (char == " ") {
+            criptogram.appendChild(word);
+            word = document.createElement("div");
+            word.setAttribute("class", "word");
+        }
+        else {
+            const letter = addLetter(char);
+            word.appendChild(letter);
+        }
+    });
+    criptogram.appendChild(word);
+    criptogramContainer === null || criptogramContainer === void 0 ? void 0 : criptogramContainer.appendChild(criptogram);
+    console.log(phrase);
+    console.log(phraseChars);
+    printKey();
+};
+const printKey = () => {
+    const key = document.getElementById("key");
+    while (key === null || key === void 0 ? void 0 : key.firstChild) {
+        key.removeChild(key.firstChild);
+    }
+    ;
+    charsIcons.forEach((icon, char) => {
+        const div = document.createElement("div");
+        const letter = document.createElement("h6");
         letter.innerHTML += char;
-        var image = document.createElement("img");
-        image.setAttribute("src", "./icons/".concat(icon));
+        const image = document.createElement("img");
+        image.setAttribute("src", `./icons/${icon}`);
         div.appendChild(letter);
         div.appendChild(image);
         key === null || key === void 0 ? void 0 : key.appendChild(div);
     });
 };
-var btnTest = window.document.getElementById("btn-test");
-btnTest === null || btnTest === void 0 ? void 0 : btnTest.addEventListener("click", printKey);
+const addLetter = (char) => {
+    const div = document.createElement("div");
+    const letter = document.createElement("h6");
+    // letter.innerHTML += char;
+    const image = document.createElement("img");
+    const icon = charsIcons.get(char);
+    image.setAttribute("src", `./icons/${icon}`);
+    div.appendChild(letter);
+    div.appendChild(image);
+    return div;
+};
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 
-const container = window.document.getElementById("container");
+const container = document.getElementById("container");
 
 const charCodes: readonly number[] = Array.from(Array(26)).map((_element, index) => index + 65);
 const alphabet: readonly string[] = charCodes.map(charCode => String.fromCharCode(charCode));
@@ -14,8 +14,39 @@ const charsIcons: Map<string, string> = new Map();
 alphabet.forEach((char, index) => charsIcons.set(char, icons[index]));
 console.log(charsIcons);
 
+document.getElementById("form")?.addEventListener("submit", (event) => { 
+    event.preventDefault();
+    generateCriptogram();
+});
+
+const generateCriptogram = () => {
+    const phrase: string = (document.getElementById("input-phrase") as HTMLInputElement)?.value.toUpperCase();
+    const phraseChars: string[] = [...phrase];
+    const criptogramContainer = document.getElementById("criptogram-container");
+    const criptogram = document.createElement("div");
+    criptogram.setAttribute("class", "criptogram");
+    let word = document.createElement("div");
+    word.setAttribute("class", "word");
+    phraseChars.forEach(char => {
+        if (char == " ") {
+            criptogram.appendChild(word);
+            word = document.createElement("div");
+            word.setAttribute("class", "word");
+        } else {
+            const letter = addLetter(char);
+            word.appendChild(letter);
+        }
+    });
+    criptogram.appendChild(word);
+    criptogramContainer?.appendChild(criptogram);
+    console.log(phrase);
+    console.log(phraseChars);
+    printKey();
+}
+
 const printKey = () => {
     const key = document.getElementById("key");
+    while (key?.firstChild) { key.removeChild(key.firstChild) };
     charsIcons.forEach((icon, char) => {
         const div = document.createElement("div");
         const letter = document.createElement("h6");
@@ -28,7 +59,14 @@ const printKey = () => {
     });
 }
 
-const btnTest = window.document.getElementById("btn-test");
-btnTest?.addEventListener("click", printKey)
-
-
+const addLetter = (char: string) => {
+    const div = document.createElement("div");
+    const letter = document.createElement("h6");
+    // letter.innerHTML += char;
+    const image = document.createElement("img");
+    const icon = charsIcons.get(char);
+    image.setAttribute("src", `./icons/${icon}`);
+    div.appendChild(letter);
+    div.appendChild(image);
+    return div;
+}
