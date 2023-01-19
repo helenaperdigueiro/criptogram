73b561e0fb72373749cbd745ceee4d6a4b12476e
Helena Perdigueiro <helena.perdigueiro@gmail.com>
2023-01-19 15:26:24 -0300

    added shuffle key and delete all btns

--- build/main.js
+++ build/main.js
@@ -1,93 +1,108 @@
 "use strict";
-// (function () {
-var _a;
-const container = document.getElementById("container");
-const charCodes = Array.from(Array(26)).map((_element, index) => index + 65);
-const alphabet = charCodes.map(charCode => String.fromCharCode(charCode));
-const icons = ["avocado.png", "baguette.png", "banana.png", "barley.png", "biscuit.png", "burger.png", "capsicum.png", "carrot.png", "cauliflower.png",
-    "cheese-slice.png", "chilli.png", "chocolate.png", "cutlery.png", "disposable.png", "donut.png", "eggplant.png", "fried-fish.png", "honey-dipper.png", "ice-cream.png",
-    "lobster.png", "peas.png", "pizza-slice.png", "toaster.png", "turkey.png", "turnip.png", "waffle.png"];
-const charsIcons = new Map();
-alphabet.forEach((char, index) => charsIcons.set(char, icons[index]));
-(_a = document.getElementById("form")) === null || _a === void 0 ? void 0 : _a.addEventListener("submit", (event) => {
-    event.preventDefault();
-    generateCriptogram();
-});
-const criptogramsContainer = document.getElementById("criptograms-container");
-const generateCriptogram = () => {
-    var _a;
-    const phrase = (_a = document.getElementById("input-phrase")) === null || _a === void 0 ? void 0 : _a.value.toUpperCase();
-    const phraseChars = [...phrase];
-    const criptogramContainer = document.createElement("div");
-    criptogramContainer.setAttribute("class", "criptogram-container");
-    const criptogramBtns = document.createElement("div");
-    criptogramBtns.setAttribute("class", "criptogram-btns");
-    const btnDeleteCriptogram = document.createElement("img");
-    btnDeleteCriptogram.setAttribute("class", "btn-delete-criptogram");
-    btnDeleteCriptogram.setAttribute("src", "./icons/btns/cancel.png");
-    btnDeleteCriptogram.addEventListener("click", () => criptogramContainer.remove());
-    criptogramBtns.appendChild(btnDeleteCriptogram);
-    criptogramContainer.appendChild(criptogramBtns);
-    const criptogram = document.createElement("div");
-    criptogram.setAttribute("class", "criptogram");
-    let word = document.createElement("div");
-    word.setAttribute("class", "word");
-    let letter;
-    phraseChars.forEach(char => {
-        if (char == " ") {
-            criptogram.appendChild(word);
-            word = document.createElement("div");
+(function () {
+    const init = () => {
+        var _a;
+        const container = document.getElementById("container");
+        const charCodes = Array.from(Array(26)).map((_element, index) => index + 65);
+        const alphabet = charCodes.map(charCode => String.fromCharCode(charCode));
+        const icons = ["avocado.png", "baguette.png", "banana.png", "barley.png", "biscuit.png", "burger.png", "capsicum.png", "carrot.png", "cauliflower.png",
+            "cheese-slice.png", "chilli.png", "chocolate.png", "cutlery.png", "disposable.png", "donut.png", "eggplant.png", "fried-fish.png", "honey-dipper.png", "ice-cream.png",
+            "lobster.png", "peas.png", "pizza-slice.png", "toaster.png", "turkey.png", "turnip.png", "waffle.png"];
+        let currentKey = new Map();
+        const shuffleKey = () => {
+            alphabet.forEach((char, index) => currentKey.set(char, icons[Math.floor(Math.random() * icons.length)]));
+        };
+        (_a = document.getElementById("form")) === null || _a === void 0 ? void 0 : _a.addEventListener("submit", (event) => {
+            event.preventDefault();
+            generateCriptogram();
+        });
+        const criptogramsContainer = document.getElementById("criptograms-container");
+        const generateCriptogram = () => {
+            const input = document.getElementById("input-phrase");
+            const phrase = input === null || input === void 0 ? void 0 : input.value.toUpperCase();
+            const phraseChars = [...phrase];
+            input.value = "";
+            const criptogramContainer = document.createElement("div");
+            criptogramContainer.setAttribute("class", "criptogram-container");
+            const criptogramBtns = document.createElement("div");
+            criptogramBtns.setAttribute("class", "criptogram-btns");
+            const btnDeleteCriptogram = document.createElement("img");
+            btnDeleteCriptogram.setAttribute("class", "btn-delete-criptogram");
+            btnDeleteCriptogram.setAttribute("src", "./icons/btns/delete.png");
+            btnDeleteCriptogram.addEventListener("click", () => criptogramContainer.remove());
+            criptogramBtns.appendChild(btnDeleteCriptogram);
+            criptogramContainer.appendChild(criptogramBtns);
+            const criptogram = document.createElement("div");
+            criptogram.setAttribute("class", "criptogram");
+            let word = document.createElement("div");
             word.setAttribute("class", "word");
-        }
-        else {
-            letter = addLetter(char);
-            word.appendChild(letter);
-        }
-    });
-    criptogram.appendChild(word);
-    criptogramContainer.appendChild(criptogram);
-    criptogramsContainer === null || criptogramsContainer === void 0 ? void 0 : criptogramsContainer.appendChild(criptogramContainer);
-    printKey();
-};
-const printKey = () => {
-    const key = document.getElementById("key");
-    while (key === null || key === void 0 ? void 0 : key.firstChild) {
-        key.removeChild(key.firstChild);
-    }
-    ;
-    charsIcons.forEach((icon, char) => {
-        const div = document.createElement("div");
-        const letter = document.createElement("h6");
-        letter.innerHTML += char;
-        const image = document.createElement("img");
-        image.setAttribute("src", `./icons/food-theme/${icon}`);
-        div.appendChild(letter);
-        div.appendChild(image);
-        key === null || key === void 0 ? void 0 : key.appendChild(div);
-    });
-};
-const addLetter = (char) => {
-    const div = document.createElement("div");
-    const letterContainer = document.createElement("div");
-    letterContainer.setAttribute("class", "letter-container");
-    const letter = document.createElement("h6");
-    letter.setAttribute("class", "criptogram-letter hidden");
-    letter.innerHTML += char;
-    const image = document.createElement("img");
-    const icon = charsIcons.get(char);
-    image.setAttribute("src", `./icons/food-theme/${icon}`);
-    letterContainer.appendChild(letter);
-    div.appendChild(letterContainer);
-    div.appendChild(image);
-    return div;
-};
-const btnShowPhraseCriptogram = document.getElementById("btn-show-hide");
-btnShowPhraseCriptogram === null || btnShowPhraseCriptogram === void 0 ? void 0 : btnShowPhraseCriptogram.addEventListener("click", () => {
-    let src = btnShowPhraseCriptogram.getAttribute("src") == "./icons/btns/show.png" ? "./icons/btns/hide.png" : "./icons/btns/show.png";
-    btnShowPhraseCriptogram.setAttribute("src", src);
-    const letters = Array.from(document.getElementsByClassName("criptogram-letter"));
-    letters.forEach(letter => {
-        letter.classList.toggle("hidden");
-    });
-});
-// })();
+            let letter;
+            phraseChars.forEach(char => {
+                if (char == " ") {
+                    criptogram.appendChild(word);
+                    word = document.createElement("div");
+                    word.setAttribute("class", "word");
+                }
+                else {
+                    letter = addLetter(char);
+                    word.appendChild(letter);
+                }
+            });
+            criptogram.appendChild(word);
+            criptogramContainer.appendChild(criptogram);
+            criptogramsContainer === null || criptogramsContainer === void 0 ? void 0 : criptogramsContainer.appendChild(criptogramContainer);
+        };
+        const printKey = () => {
+            shuffleKey();
+            let keyContainer = document.getElementById("key-container");
+            let key = document.getElementById("key");
+            key === null || key === void 0 ? void 0 : key.remove();
+            key = document.createElement("div");
+            key.setAttribute("id", "key");
+            currentKey.forEach((icon, char) => {
+                const div = document.createElement("div");
+                const letter = document.createElement("h6");
+                letter.innerHTML += char;
+                const image = document.createElement("img");
+                image.setAttribute("src", `./icons/food-theme/${icon}`);
+                div.appendChild(letter);
+                div.appendChild(image);
+                key === null || key === void 0 ? void 0 : key.appendChild(div);
+            });
+            keyContainer === null || keyContainer === void 0 ? void 0 : keyContainer.appendChild(key);
+        };
+        const addLetter = (char) => {
+            const div = document.createElement("div");
+            const letterContainer = document.createElement("div");
+            letterContainer.classList.add("letter-container");
+            const letter = document.createElement("h6");
+            letter.classList.add("criptogram-letter");
+            letter.innerHTML += char;
+            const image = document.createElement("img");
+            const icon = currentKey.get(char);
+            image.setAttribute("src", `./icons/food-theme/${icon}`);
+            letterContainer.appendChild(letter);
+            div.appendChild(letterContainer);
+            div.appendChild(image);
+            return div;
+        };
+        const btnShowHide = document.getElementById("btn-show-hide");
+        btnShowHide === null || btnShowHide === void 0 ? void 0 : btnShowHide.addEventListener("click", () => {
+            let src = btnShowHide.getAttribute("src") == "./icons/btns/show.png" ? "./icons/btns/hide.png" : "./icons/btns/show.png";
+            btnShowHide.setAttribute("src", src);
+            criptogramsContainer === null || criptogramsContainer === void 0 ? void 0 : criptogramsContainer.setAttribute("hidden-letters", `${!(criptogramsContainer.getAttribute("hidden-letters") == "true")}`);
+        });
+        const deleteAllCriptograms = () => {
+            criptogramsContainer === null || criptogramsContainer === void 0 ? void 0 : criptogramsContainer.replaceChildren();
+        };
+        const bntDeleteAll = document.getElementById("btn-delete-all");
+        bntDeleteAll === null || bntDeleteAll === void 0 ? void 0 : bntDeleteAll.addEventListener("click", deleteAllCriptograms);
+        const btnShuffleKey = document.getElementById("btn-shuffle-key");
+        btnShuffleKey === null || btnShuffleKey === void 0 ? void 0 : btnShuffleKey.addEventListener("click", () => {
+            deleteAllCriptograms();
+            printKey();
+        });
+        printKey();
+    };
+    window.addEventListener('load', init);
+})();
--- src/main.ts
+++ src/main.ts
@@ -1,101 +1,125 @@
-// (function () {
+(function () {
 
-    const container = document.getElementById("container");
+    const init = () => {
+        const container = document.getElementById("container");
 
-    const charCodes: readonly number[] = Array.from(Array(26)).map((_element, index) => index + 65);
-    const alphabet: readonly string[] = charCodes.map(charCode => String.fromCharCode(charCode));
+        const charCodes: readonly number[] = Array.from(Array(26)).map((_element, index) => index + 65);
+        const alphabet: readonly string[] = charCodes.map(charCode => String.fromCharCode(charCode));
 
-    const icons: readonly string[] = ["avocado.png", "baguette.png", "banana.png", "barley.png", "biscuit.png", "burger.png", "capsicum.png", "carrot.png", "cauliflower.png",
-        "cheese-slice.png", "chilli.png", "chocolate.png", "cutlery.png", "disposable.png", "donut.png", "eggplant.png", "fried-fish.png", "honey-dipper.png", "ice-cream.png",
-        "lobster.png", "peas.png", "pizza-slice.png", "toaster.png", "turkey.png", "turnip.png", "waffle.png"];
+        const icons: readonly string[] = ["avocado.png", "baguette.png", "banana.png", "barley.png", "biscuit.png", "burger.png", "capsicum.png", "carrot.png", "cauliflower.png",
+            "cheese-slice.png", "chilli.png", "chocolate.png", "cutlery.png", "disposable.png", "donut.png", "eggplant.png", "fried-fish.png", "honey-dipper.png", "ice-cream.png",
+            "lobster.png", "peas.png", "pizza-slice.png", "toaster.png", "turkey.png", "turnip.png", "waffle.png"];
 
-    const charsIcons: Map<string, string> = new Map();
-    alphabet.forEach((char, index) => charsIcons.set(char, icons[index]));
+        let currentKey: Map<string, string> = new Map();
+        const shuffleKey = () => {
+            alphabet.forEach((char, index) => currentKey.set(char, icons[Math.floor(Math.random() * icons.length)]));
+        }
 
-    document.getElementById("form")?.addEventListener("submit", (event) => {
-        event.preventDefault();
-        generateCriptogram();
-    });
-
-    const criptogramsContainer = document.getElementById("criptograms-container");
-
-    const generateCriptogram = () => {
-        const phrase: string = (document.getElementById("input-phrase") as HTMLInputElement)?.value.toUpperCase();
-        const phraseChars: string[] = [...phrase];
-        const criptogramContainer = document.createElement("div");
-        criptogramContainer.setAttribute("class", "criptogram-container");
-
-        const criptogramBtns = document.createElement("div");
-        criptogramBtns.setAttribute("class", "criptogram-btns");
-
-        const btnDeleteCriptogram = document.createElement("img");
-        btnDeleteCriptogram.setAttribute("class", "btn-delete-criptogram");
-        btnDeleteCriptogram.setAttribute("src", "./icons/btns/cancel.png");
-        btnDeleteCriptogram.addEventListener("click", () => criptogramContainer.remove());
-        criptogramBtns.appendChild(btnDeleteCriptogram);
-
-        criptogramContainer.appendChild(criptogramBtns);
-        const criptogram = document.createElement("div");
-        criptogram.setAttribute("class", "criptogram");
-        let word = document.createElement("div");
-        word.setAttribute("class", "word");
-        let letter: HTMLDivElement;
-
-        phraseChars.forEach(char => {
-            if (char == " ") {
-                criptogram.appendChild(word);
-                word = document.createElement("div");
-                word.setAttribute("class", "word");
-            } else {
-                letter = addLetter(char);
-                word.appendChild(letter);
-            }
+        document.getElementById("form")?.addEventListener("submit", (event) => {
+            event.preventDefault();
+            generateCriptogram();
         });
-        criptogram.appendChild(word);
-        criptogramContainer.appendChild(criptogram);
-        criptogramsContainer?.appendChild(criptogramContainer);
 
-        printKey();
-    }
+        const criptogramsContainer = document.getElementById("criptograms-container");
 
-    const printKey = () => {
-        const key = document.getElementById("key");
-        while (key?.firstChild) { key.removeChild(key.firstChild) };
-        charsIcons.forEach((icon, char) => {
+        const generateCriptogram = () => {
+            const input = document.getElementById("input-phrase") as HTMLInputElement;
+            const phrase: string = input?.value.toUpperCase();
+            const phraseChars: string[] = [...phrase];
+            input.value = "";
+            const criptogramContainer = document.createElement("div");
+            criptogramContainer.setAttribute("class", "criptogram-container");
+
+            const criptogramBtns = document.createElement("div");
+            criptogramBtns.setAttribute("class", "criptogram-btns");
+
+            const btnDeleteCriptogram = document.createElement("img");
+            btnDeleteCriptogram.setAttribute("class", "btn-delete-criptogram");
+            btnDeleteCriptogram.setAttribute("src", "./icons/btns/delete.png");
+            btnDeleteCriptogram.addEventListener("click", () => criptogramContainer.remove());
+            criptogramBtns.appendChild(btnDeleteCriptogram);
+
+            criptogramContainer.appendChild(criptogramBtns);
+            const criptogram = document.createElement("div");
+            criptogram.setAttribute("class", "criptogram");
+            let word = document.createElement("div");
+            word.setAttribute("class", "word");
+            let letter: HTMLDivElement;
+
+            phraseChars.forEach(char => {
+                if (char == " ") {
+                    criptogram.appendChild(word);
+                    word = document.createElement("div");
+                    word.setAttribute("class", "word");
+                } else {
+                    letter = addLetter(char);
+                    word.appendChild(letter);
+                }
+            });
+            criptogram.appendChild(word);
+            criptogramContainer.appendChild(criptogram);
+            criptogramsContainer?.appendChild(criptogramContainer);
+
+        }
+
+        const printKey = () => {
+            shuffleKey();
+            let keyContainer = document.getElementById("key-container");
+            let key = document.getElementById("key");
+            key?.remove();
+            key = document.createElement("div");
+            key.setAttribute("id", "key");
+            currentKey.forEach((icon, char) => {
+                const div = document.createElement("div");
+                const letter = document.createElement("h6");
+                letter.innerHTML += char;
+                const image = document.createElement("img");
+                image.setAttribute("src", `./icons/food-theme/${icon}`);
+                div.appendChild(letter);
+                div.appendChild(image);
+                key?.appendChild(div);
+            });
+            keyContainer?.appendChild(key);
+        }
+
+        const addLetter = (char: string) => {
             const div = document.createElement("div");
+            const letterContainer = document.createElement("div");
+            letterContainer.classList.add("letter-container")
             const letter = document.createElement("h6");
+            letter.classList.add("criptogram-letter")
             letter.innerHTML += char;
             const image = document.createElement("img");
+            const icon = currentKey.get(char);
             image.setAttribute("src", `./icons/food-theme/${icon}`);
-            div.appendChild(letter);
+            letterContainer.appendChild(letter);
+            div.appendChild(letterContainer);
             div.appendChild(image);
-            key?.appendChild(div);
-        });
-    }
+            return div;
+        }
 
-    const addLetter = (char: string) => {
-        const div = document.createElement("div");
-        const letterContainer = document.createElement("div");
-        letterContainer.setAttribute("class", "letter-container");
-        const letter = document.createElement("h6");
-        letter.setAttribute("class", "criptogram-letter hidden")
-        letter.innerHTML += char;
-        const image = document.createElement("img");
-        const icon = charsIcons.get(char);
-        image.setAttribute("src", `./icons/food-theme/${icon}`);
-        letterContainer.appendChild(letter);
-        div.appendChild(letterContainer);
-        div.appendChild(image);
-        return div;
-    }
-
-    const btnShowPhraseCriptogram = document.getElementById("btn-show-hide");
-    btnShowPhraseCriptogram?.addEventListener("click", () => {
-        let src = btnShowPhraseCriptogram.getAttribute("src") == "./icons/btns/show.png" ? "./icons/btns/hide.png" : "./icons/btns/show.png";
-        btnShowPhraseCriptogram.setAttribute("src", src);
-        const letters = Array.from(document.getElementsByClassName("criptogram-letter"));
-        letters.forEach(letter => {
-            letter.classList.toggle("hidden");
+        const btnShowHide = document.getElementById("btn-show-hide");
+        btnShowHide?.addEventListener("click", () => {
+            let src = btnShowHide.getAttribute("src") == "./icons/btns/show.png" ? "./icons/btns/hide.png" : "./icons/btns/show.png";
+            btnShowHide.setAttribute("src", src);
+            criptogramsContainer?.setAttribute("hidden-letters", `${!(criptogramsContainer.getAttribute("hidden-letters") == "true")}`);
         })
-    })
-// })();
+
+        const deleteAllCriptograms = () => {
+            criptogramsContainer?.replaceChildren();
+        }
+        const bntDeleteAll = document.getElementById("btn-delete-all");
+        bntDeleteAll?.addEventListener("click", deleteAllCriptograms);
+
+        const btnShuffleKey = document.getElementById("btn-shuffle-key");
+        btnShuffleKey?.addEventListener("click", () => {
+            deleteAllCriptograms();
+            printKey();
+        });
+
+        printKey();
+    };
+
+    window.addEventListener('load', init);
+
+})();
